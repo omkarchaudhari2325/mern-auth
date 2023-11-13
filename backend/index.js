05a29272c99ec96b08a72aa8d1ab061eb5e97293
omkarchaudhari2325 <chaudhariomkar369@gmail.com>
2023-11-13 15:45:51 +0530

    created routes

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ import express from "express"
 import dotenv from "dotenv"
 import dbConnect from "./config/db_connect.js"
 dotenv.config();
+import userRoute from "./routes/route.js"
 const PORT = process.env.PORT || 3000
 const app = express();
 dbConnect();
+
+app.use("/api/user",userRoute);
 app.listen(PORT,() =>{
     console.log("server is running at port " + PORT)
 })
