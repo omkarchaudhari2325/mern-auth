8301847dedca5e7faa890b957f9d6a02fd14c034
omkarchaudhari2325 <chaudhariomkar369@gmail.com>
2023-11-13 20:37:58 +0530

    completed the signin page functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -56,11 +56,17 @@ export const signin = async (req,res,next) =>{
     try{
         const validUser = await User.findOne({email});
         if(!validUser) {
-            return next(errorHandler(404,"User not valid"))
+            return res.status(401).json({
+                success : false,
+                message : "Please register first"
+            })
         }
         const validPassword = bcrypt.compareSync(password , validUser.password);
         if(!validPassword){
-            return next(errorHandler(401,"Wrong Credentails"));
+            return res.status(403).json({
+                success : false,
+                message : "Invalid Credentials Entered"
+            })
         }
         validUser.password = undefined;
         const token = jwt.sign({id : validUser._id},process.env.JWT_SECRET,{
