917c7d94c5e607413f602a221d1bd60f75bd28af
omkarchaudhari2325 <chaudhariomkar369@gmail.com>
2023-11-13 16:27:50 +0530

    completed signUp page UI

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,13 @@
 import User from "../models/User.js"
 import bcrypt from "bcryptjs"
+import { errorHandler } from "../utils/error.js";
 export const signup = async (req,res) =>{
     // console.log(req.body);
     const {username,email,password} = req.body;
     const hashedPassword = await bcrypt.hash(password,10);
     try{
-            const existingUser = User.findOne({email});
+            const existingUser = await User.findOne({email});
+            console.log(existingUser)
             if(existingUser){
                 return res.status(500).json({
                     success : false,
@@ -16,13 +18,14 @@ export const signup = async (req,res) =>{
                                         email,
                                          password : hashedPassword});
             await newUser.save();
-            return res.status(200).json({
+             res.status(200).json({
             success : true,
             message : "User created Succesfully",
             user : newUser
      })
     }
     catch(err){
+        // return res.errorHandler(500,"Something error happened")
         return res.status(500).json({
             success : false,
             message : err.message
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+// import Link from "react-router-dom"
+import { Link } from 'react-router-dom'
 const SignUp = () => {
   return (
-    <div>
-      SignUp
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold'>
+        Sign Up
+      </h1>
+      <form action="" className='flex flex-col gap-4 my-7'>
+        <input type="text" placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg '  />
+        <input type="email" placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg'  />
+        <input type="password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg '  />
+        <button className='bg-slate-700 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 text-white p-3 '>Sign Up</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+      <Link to= "/sign-in">
+        <p>Have an account</p>
+
+      </Link>
+
+      </div>
     </div>
   )
 }
